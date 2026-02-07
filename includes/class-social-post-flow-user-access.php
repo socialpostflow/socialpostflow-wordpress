4b4176e60ac2e67b34cf618637083daf28ca9506
Tim Carr <tim@n7studios.co.uk>
2026-02-07 13:45:55 +0800

    Added: Plugins: Display notice if trial ended

--- includes/class-social-post-flow-user-access.php
+++ includes/class-social-post-flow-user-access.php
@@ -31,7 +31,29 @@ class Social_Post_Flow_User_Access {
 	 */
 	public function __construct() {
 
+		add_action( 'after_plugin_row_social-post-flow/social-post-flow.php', array( $this, 'add_user_no_access_notice_to_plugins_screen' ), 10, 1 );
 		add_filter( 'social_post_flow_notices_get_notices', array( $this, 'add_user_no_access_notice' ) );
+
+	}
+
+	/**
+	 * Adds a notice in the Plugins screen if the user does not have access to Social Post Flow,
+	 * linking them to the checkout page or the billing page.
+	 *
+	 * @since   1.2.1
+	 */
+	public function add_user_no_access_notice_to_plugins_screen() {
+
+		// Bail if the user has access.
+		if ( ! $this->user_has_no_access() ) {
+			return;
+		}
+
+		// Output the notice.
+		printf(
+			'<tr class="plugin-update-tr active" id="social-post-flow" data-slug="social-post-flow" data-plugin="social-post-flow/social-post-flow.php"><td colspan="4" class="plugin-update colspanchange"><div class="update-message notice inline notice-error notice-alt"><p>%s</p></div></td></tr>',
+			wp_kses_post( $this->get_notice() )
+		);
 
 	}
 
@@ -51,21 +73,8 @@ class Social_Post_Flow_User_Access {
 			return $notices;
 		}
 
-		// Get user transient, which was set when a call to user() was made via the API class.
-		// This prevents querying the API on every request.
-		$user = get_transient( 'social_post_flow_api_user' );
-
 		// Add the notice.
-		$notices['error'][] = sprintf(
-			'<strong>%s:</strong> %s <a href="%s" target="_blank">%s</a> %s<br /><a href="%s">%s</a>',
-			__( 'Social Post Flow', 'social-post-flow' ),
-			__( 'Your trial has ended. A paid subscription is required for continued use.', 'social-post-flow' ),
-			social_post_flow()->get_class( 'api' )->get_billing_url(),
-			__( 'Purchase a plan', 'social-post-flow' ),
-			__( 'to resume posting to social media.', 'social-post-flow' ),
-			admin_url( 'admin.php?page=social-post-flow' ),
-			__( 'I\'ve already done this', 'social-post-flow' )
-		);
+		$notices['error'][] = $this->get_notice();
 
 		// Return notices.
 		return $notices;
@@ -121,6 +130,8 @@ class Social_Post_Flow_User_Access {
 	 * i.e. their trial ended and no paid plan is active.
 	 *
 	 * @since   1.1.7
+	 *
+	 * @return  bool
 	 */
 	public function user_has_no_access() {
 
@@ -137,6 +148,28 @@ class Social_Post_Flow_User_Access {
 	public function delete_user_no_access_flag() {
 
 		delete_option( $this->settings_name );
+
+	}
+
+	/**
+	 * Returns the notice text.
+	 *
+	 * @since   1.2.2
+	 *
+	 * @return  string
+	 */
+	private function get_notice() {
+
+		return sprintf(
+			'<strong>%s:</strong> %s <a href="%s" target="_blank">%s</a> %s<br /><a href="%s">%s</a>',
+			__( 'Social Post Flow', 'social-post-flow' ),
+			__( 'Your trial has ended. A paid subscription is required for continued use.', 'social-post-flow' ),
+			social_post_flow()->get_class( 'api' )->get_billing_url(),
+			__( 'Purchase a plan', 'social-post-flow' ),
+			__( 'to resume posting to social media.', 'social-post-flow' ),
+			admin_url( 'admin.php?page=social-post-flow' ),
+			__( 'I\'ve already done this', 'social-post-flow' )
+		);
 
 	}
 
